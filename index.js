a5ae86a658562676255c762262fed3f9e0901b94
Nate Long <nate.k.long@gmail.com>
2023-03-29 10:59:40 -0400

    syntax index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const connection = require('./db/connection');
 
-// Server Start after sqlDb connection
+// Server Start after connection made
 db.connect(err => {
   if (err) throw err;
   console.log('Database connected!');
